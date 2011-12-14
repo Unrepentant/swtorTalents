8f54b072d4e59ec04cf8ee0005d3072d7b3c2bbd
Rob Garrison <wowmotty@gmail.com>
2011-12-13 22:14:40 -0600

    initial commit

---
+++ README.markdown
@@ -0,0 +1,148 @@
+## SWTORTalents (Recruit by talent) ##
+
+Show your Star Wars the Old Republic recruitment status by displaying the specific talent needed for each class.
+
+ ![different views](http://mottie.github.com/swtorTalents/demo/views.jpg)
+
+## Features ([Demo](http://mottie.github.com/swtorTalents/)) ##
+
+* This World of Warcraft class recruitment box shows the class needs by talent tree specialization.
+* Icons are set to fade out as the need requirements lessen (set in the CSS).
+* Tooltips show the need level with either the tooltip background or the text colored specifically to the need. See screen shot above, with `colorBackground` set to `true` and `false`.
+* Minimal coding is required to set the needs of any one class.
+
+## Requirements ##
+
+* jQuery 1.2.6+
+* Tooltip plugin (uses metadata & live events) - included with script
+
+## Default Options ##
+**Script**
+
+```javascript
+$(function(){
+
+  $('.recruitTalent').wowTalents({
+    side            : 'empire',  // Empire or Republic? case insensitive and only the first letter is required
+    defaultNeed     : 'Low',     // Default need that is set when no setting is found
+    colorBackground : true,      // Adds need level color to the tooltip background (true) or the text (false)
+    tooltipClass    : 'tooltip', // Class added for tooltip script to target
+    tooltipWidth    : 150,       // Tooltip width, added to the tooltip metadata
+    tooltipMetadata : 'class',   // Location the tooltip meta data is added e.g. {width:150px;color:#ddd;background:#333;}
+    addHeader       : true       // Add headers between groups; set to false for compact view
+
+    /* Empire specific options */
+    juggernaught    : "x,x",
+    maurader        : "x,x",
+    assassin        : "x,x",
+    sorceror        : "x,x",
+    powertech       : "x,x",
+    mercenary       : "x,x",
+    operative       : "x,x",
+    sniper          : "x,x",
+
+    /* Republic specific options */
+    guardian        : "x,x",
+    sentinel        : "x,x",
+    sage            : "x,x",
+    shadow          : "x,x",
+    scoundrel       : "x,x",
+    gunslinger      : "x,x",
+    commando        : "x,x",
+    vanguard        : "x,x",
+
+    displayNames    : {   // change the need level displayed in the tooltip
+     'None'   : 'None',   // e.g. 'None' : 'Closed'
+     'Low'    : 'Low',    // the specific options above will still use "x", "n", "l", "m" and "h"
+     'Medium' : 'Medium', // Look under the options section below for an expanded example
+     'High'   : 'High'
+    }
+
+  });
+
+});
+```
+
+**HTML**
+
+```html
+<div class="recruitTalent"></div>
+```
+
+## Customization ##
+
+**CSS Styles:**
+
+* `.recruitTitle` - Styles the title of the widget above the box. The text of this title is in blue, look for "Recruitment Needs".
+* `.recruitTalent` - Sets the overall style of the widget. Set to be 200px wide by default.
+* `.high` - High priority text color (green by default). The tooltip background color is also set from this text color.
+* `.medium` - Medium priority text color (yellow by default).
+* `.low` - Low priority text color (red by default).
+* `.none` - No needs text color (grey by default).
+* `.usebg` - This sets the text color when the tooltip background color is being changed (black by default). Without this the text would be the same as the `#tooltip` definition.
+* `.talentIcons .high`, `.talentIcons .medium`, `.talentIcons .low` and `.talentIcons .none` - sets the opacity of the icon at each priority
+* `.classIcon` - Styles the table cell that contains the class icon and class text. It also contains the background image sprite which contains an image of all the WoW classes
+* `.classIcon span` - Styles only the class text. It was included to add padding between the icon and the text.
+* `.talentIcons` - Styles the table cell that contains all three talent icons.
+* `.talentIcon` - Contains the background image which contains all of the talent tree images.
+
+**Options:**
+
+* `defaultNeed` is set to "Low" by default
+What this variable does is if you don't set the specific class need in the options, it will default to whatever this variable is set to.
+You can set this variable to `l` (low), `m` (medium) or `h` (high), anything else defaults to `n` (none).
+
+* `colorBackground` is set to true by default. When true, hovering over a talent tree icon will display a tooltip with the background color set.
+
+* `addHeader` is set to true by default. When set to true, the header images are added (seen in the non-compact screen shots above). If set to false, the compact view will be seen.
+
+* `tooltipWidth` is set to 150 by default (in pixels). You can adjust this to fit the tooltip contents. The contents are centered within the coding.
+
+* `displayNames` allows you to change the name/language of the need levels. For example, to change the "None" to "Closed", do the following:
+
+    ```javascript
+    $('.recruitTalent').wowTalents({
+      // the defaultNeed should still be set to the original name, like "None", NOT "Closed"
+      defaultNeed     : 'None',
+
+      // when adding the talent need level, use "n" for "Closed"
+      // and "h" for "Open" to show up in the tooltip
+      juggernaught    : "n,h",
+
+      // Change the level shown in the tooltip as to open and closed.
+      displayNames    : {
+        'None' : 'Closed',
+        'High' : 'Open'
+      }
+    });
+    ```
+
+* Use the class name to set each class need. Replace the `x` below with an `n` (none), `l` (low), `m` (medium) or `h` (high); adding anything else will cause the script to add the value of the `defaultNeed` option:
+
+```javascript
+/* Empire specific options */
+juggernaught : "x,x",
+maurader     : "x,x",
+assassin     : "x,x",
+sorceror     : "x,x",
+powertech    : "x,x",
+mercenary    : "x,x",
+operative    : "x,x",
+sniper       : "x,x",
+
+/* Republic specific options */
+guardian     : "x,x",
+sentinel     : "x,x",
+sage         : "x,x",
+shadow       : "x,x",
+scoundrel    : "x,x",
+gunslinger   : "x,x",
+commando     : "x,x",
+vanguard     : "x,x"
+```
+
+##Changelog##
+
+###**Version 1.0 (12/13/2011)**
+
+* Initial version
---
+++ js/swtortalents.js
@@ -0,0 +1,202 @@
+/* Star Wars the Old Republic Recruit By Talent Widget
+ * by Rob G (Mottie) 2011
+ * http://mottie.guildportal.com
+ * licensed under the MIT license.
+ */
+
+(function($){
+ $.swtorTalents = function(el, options){
+  var o, base = this;
+  base.$el = $(el);
+  base.$el.data('swtorTalents', base);
+
+  base.init = function(){
+   base.options = o = $.extend({}, $.swtorTalents.defaultOptions, options);
+
+   var c,i,j,k,t,tal,
+   s = (/^e/i.test(o.side)) ? 'empire' : 'republic', // added to class - empire or republic?
+
+   // Validate defaultNeed class
+   n = o.defaultNeed.toLowerCase();
+   o.defaultNeed = "None";
+   if (/^l/.test(n)) { o.defaultNeed = 'Low'; }
+   if (/^m/.test(n)) { o.defaultNeed = 'Medium'; }
+   if (/^h/.test(n)) { o.defaultNeed = 'High'; }
+
+   // To set tooltip background colors, we need to append objects
+   // with the assigned class to get the CSS text color settings
+   t = $('<div id="testsetcolors" style="display:none;"><p class="usebg"/><p class="none"/><p class="low"/><p class="medium"/><p class="high"/></div>');
+   t.appendTo($('body'));
+   o.bg = t.find('.usebg').css('color'); // text color when tooltip background is colored
+   o.cn = t.find('.none').css('color');
+   o.cl = t.find('.low').css('color');
+   o.cm = t.find('.medium').css('color');
+   o.ch = t.find('.high').css('color');
+   o.cd = t.find('.' + o.defaultNeed.toLowerCase()).css('color');
+   t.remove();
+
+   // Make table
+   // **********
+   t = '<div>';
+   // if side starts with an "e" then it's empire, all else is republic
+   tal = $.swtorTalents[s];
+   for (i in tal) {
+    if (tal[i][0].hasOwnProperty('tree')){
+     t += (o.addHeader) ? '<div class="groupIcon ' + s + 'GroupIcon icon_' + i.toLowerCase().replace(/\s+/g,'') + '">'+i+'</div>' : '';
+     for (k=0; k < 2; k++){
+      n = (o[tal[i][k].name.toLowerCase().replace(/\s+/g,'')] || ',').split(',');
+      t += '<span class="' + s + 'ClassIcon classIcon ' + tal[i][k].icon + '"><span>';
+      t += tal[i][k].name + '</span></span><span class="talentIcons">';
+
+      // build tree x2 for each class
+      for ( j=0; j<2; j++ ) {
+       c = base.getStyle( n[j] || '' ); // get class c[0] & hex values c[1]
+       // tooltip width, text color and background color added as metadata into the selected attribute
+       t += '<span class="' + c[0].toLowerCase() + '"><span class="' + s + 'TalentIcon talentIcon ' + o.tooltipClass + ' ' +
+        tal[i][k].tree[j][1] + ' ' + c[0].toLowerCase() + (o.tooltipMetadata === 'class' ? ' ' : '" ' + o.tooltipMetadata + '="') +
+        '{width:' + o.tooltipWidth + 'px;color:' + (o.colorBackground ? o.bg + ';background:' : '') + c[1] +
+        ';}" title="<div style=text-align:center>' + tal[i][k].tree[j][0] + ': ' + c[0] + '</div>"></span></span> ';
+      }
+      t += '</span><br class="clear">';
+     }
+    }
+   }
+   t += '</div>';
+   base.$el.html(t);
+  };
+
+  // Return class & hex color
+  base.getStyle = function(n){
+   n = n.toLowerCase();
+   if (/^n/.test(n)) { return [o.displayNames.None, o.cn]; }
+   if (/^l/.test(n)) { return [o.displayNames.Low, o.cl]; }
+   if (/^m/.test(n)) { return [o.displayNames.Medium, o.cm]; }
+   if (/^h/.test(n)) { return [o.displayNames.High, o.ch]; }
+   return [o.displayNames[o.defaultNeed], o.cd];
+  };
+
+  // Run initializer
+  base.init();
+ };
+
+ $.swtorTalents.defaultOptions = {
+  side            : 'empire',  // Empire or Republic?
+  defaultNeed     : 'Low',     // Default need that is set when no setting is found
+  colorBackground : true,      // Adds need level color to the tooltip background (true) or the text (false)
+  tooltipClass    : 'tooltip', // Class added for tooltip script to target
+  tooltipWidth    : 150,       // Tooltip width, added to the tooltip metadata
+  tooltipMetadata : 'class',   // Location the tooltip meta data is added e.g. {width:150px;color:#ddd;background:#333;} - set to "rel" for GuildPortal version
+  addHeader       : true,      // Add headers between groups
+  displayNames    : {
+   'None'   : 'None',
+   'Low'    : 'Low',
+   'Medium' : 'Medium',
+   'High'   : 'High'
+  }
+  /* options used, but not listed; empty options will revert to the default need
+  (Empire)   juggernaught, maurader, assassin, sorceror, powertech, mercenary, operative, sniper
+  (Republic) guardian, sentinel, sage, shadow, scoundrel, gunslinger, commando, vanguard
+  */
+ };
+
+ $.swtorTalents.empire = {
+  "Sith Warrior": [{
+    "name": "Juggernaught",
+    "icon": "icon_juggernaut",
+    "tree": [[ "Vengeance", "icon_juggernaught_veng" ],
+            [ "Immortal", "icon_juggernaught_immort" ]]
+  },{
+    "name": "Maurader",
+    "icon": "icon_maurader",
+    "tree": [[ "Annihilation", "icon_maurader_annihilation" ],
+            [ "Carnage", "icon_maurader_carnage" ]]
+  }],
+  "Sith Inquisitor": [{
+    "name": "Assassin",
+    "icon": "icon_assassin",
+    "tree": [[ "Deception", "icon_assassin_deception" ],
+            [ "Darkness", "icon_assassin_darkness" ]]
+  },{
+    "name": "Sorceror",
+    "icon": "icon_sorceror",
+    "tree": [[ "Corruption", "icon_sorceror_corruption" ],
+            [ "Lightning", "icon_sorceror_lightning" ]]
+  }],
+  "Bounty Hunter": [{
+    "name": "Powertech",
+    "icon": "icon_powertech",
+    "tree": [[ "Shield Tech", "icon_powertech_shield_tech" ],
+            [ "Advanced Prototype", "icon_powertech_advanced_prototype" ]]
+  },{
+    "name": "Mercenary",
+    "icon": "icon_mercenary",
+    "tree": [[ "Arsenal", "icon_mercenary_arsenal" ],
+            [ "Bodyguard", "icon_mercenary_bodyguard" ]]
+  }],
+  "Imperial Agent": [{
+    "name": "Operative",
+    "icon": "icon_operative",
+    "tree": [[ "Concealment", "icon_operative_concealment" ],
+            [ "Medic", "icon_operative_medic" ]]
+  },{
+    "name": "Sniper",
+    "icon": "icon_sniper",
+    "tree": [[ "Marksmanship", "icon_sniper_marksmanship" ],
+            [ "Engineering", "icon_sniper_engineering" ]]
+  }]
+ };
+
+ $.swtorTalents.republic = {
+  "Jedi Knight": [{
+    "name": "Guardian",
+    "icon": "icon_guardian",
+    "tree": [[ "Defense", "icon_guardian_defense" ],
+            [ "Vigilance", "icon_guardian_vigilance" ]]
+  },{
+    "name": "Sentinel",
+    "icon": "icon_sentinel",
+    "tree": [[ "Watchman", "icon_sentinel_watchman" ],
+            [ "Combat", "icon_sentinel_combat" ]]
+  }],
+  "Jedi Consular": [{
+    "name": "Sage",
+    "icon": "icon_sage",
+    "tree": [[ "Seer", "icon_sage_seer" ],
+            [ "Telekinetic", "icon_sage_telekinetic" ]]
+  },{
+    "name": "Shadow",
+    "icon": "icon_shadow",
+    "tree": [[ "Kinetic Combat", "icon_shadow_kinetic_combat" ],
+            [ "Infiltration", "icon_shadow_infiltration" ]]
+  }],
+  "Smuggler": [{
+    "name": "Scoundrel",
+    "icon": "icon_scoundrel",
+    "tree": [[ "Scrapper", "icon_scoundrel_scrapper" ],
+            [ "Sawbones", "icon_scoundrel_sawbones" ]]
+  },{
+    "name": "Gunslinger",
+    "icon": "icon_gunslinger",
+    "tree": [[ "Sharpshooter", "icon_gunslinger_sharpshooter" ],
+            [ "Sabeteur", "icon_gunslinger_sabeteur" ]]
+  }],
+  "Trooper": [{
+    "name": "Commando",
+    "icon": "icon_commando",
+    "tree": [[ "Gunnery", "icon_commando_gunnery" ],
+            [ "Combat Medic", "icon_commando_combat_medic" ]]
+  },{
+    "name": "Vanguard",
+    "icon": "icon_vanguard",
+    "tree": [[ "Shield Specialist", "icon_vanguard_shield_specialist" ],
+            [ "Tactics", "icon_vanguard_tactics" ]]
+  }]
+ };
+
+ $.fn.swtorTalents = function(options){
+  return this.each(function(){
+   (new $.swtorTalents(this, options));
+  });
+ };
+
+})(jQuery);
